8b762533501af19dac2ca754a048c7d4b3e6898a
Kirthi Nandi <kirthi.nandi@gmail.com>
2022-07-19 15:30:06 -0700

    commit tree 09ff4a4dc99e191315dbdedff6aee147c6fcb6f6

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop(cats.length - 1);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(0);
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats, 'Broom'];
+    return copyOfCats;
+
+}
+
+function prependCat(name) {
+    const copyOfCats = ['Arnold', ...cats];
+    return copyOfCats;
+
+}
+
+function removeLastCat() {
+    const copyOfCats = cats.slice(0, cats.length - 1);
+    return copyOfCats;
+}
+
+function removeFirstCat(name) {
+    const copyOfCats = cats.slice(1);
+    return copyOfCats;
+}
